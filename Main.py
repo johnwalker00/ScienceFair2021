50d940216796b49297b671efdccc1c470b8c763d
AtomSmasher28 <33180708+AtomSmasher28@users.noreply.github.com>
2021-01-09 16:10:03 -0600

    Mostly working

--- Main.py
+++ Main.py
@@ -1,17 +1,37 @@
 from Gorithms import *
 import tkinter as tk
 
+last_common_result = ''
+last_random_result = ''
+
+def handle_click():
+    if password_ent.get() == '':
+        return
+
+    password_ent.config(state=tk.DISABLED)
+    btn.config(state=tk.DISABLED)
+
+    last_common_result = test_commons(password_ent.get())
+
+    results_lbl.config(text=f'Common Password Alg: {last_common_result}\nRandom Alg: {last_random_result}')
+    password_ent.config(state=tk.NORMAL)
+    btn.config(state=tk.NORMAL)
+
+
 window = tk.Tk()
 window.resizable(False, False)
-window.title('Title')
+window.title('Password Guesser')
 
-txt_lbl = tk.Label(master=window, height=5, text='Common Password Alg: \nRandom Alg:', justify=tk.LEFT)
-txt_lbl.grid(row=0, column=0)
+results_lbl = tk.Label(master=window, height=5, text='', justify=tk.LEFT)
+results_lbl.grid(row=0)
 
-txt_ent = tk.Entry(master=window)
-txt_ent.grid(row=1, column=0, padx=20, pady=20)
+password_ent = tk.Entry(master=window)
+password_ent.grid(row=1, column=0, padx=20, pady=10)
 
-btn = tk.Button(master=window, relief=tk.RAISED, text='Start Guessing')
+timeout_ent = tk.Entry(master=window)
+timeout_ent.grid(row=2, column=0, padx=20, pady=10)
+
+btn = tk.Button(master=window, relief=tk.RAISED, text='Start Guessing', command=handle_click)
 btn.grid(row=1, column=1, padx=20, pady=20)
 
 window.mainloop()
