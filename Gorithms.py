43fd78bc090e205dc872435b4efe601d75227ed6
AtomSmasher28 <33180708+AtomSmasher28@users.noreply.github.com>
2021-01-09 17:29:24 -0600

    Work on word+num now

--- Gorithms.py
+++ Gorithms.py
@@ -1,59 +1,87 @@
 from datetime import datetime
 
+#Helpful
+def now_seconds():
+    return datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second
+
+#Looks for a word in commonwords.txt
 def test_commons(password: str):
-    running = True
-    start_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second
+    #Log start time
+    start_time = now_seconds()
+
+    #Open file, copy it to a list, and close it
     f = open('commonwords.txt', encoding='utf8')
     words = f.readlines()
     f.close()
     
+    #Initialize loop
     success = False
     current_guess = 0
     while True:
+        #Ensure we haven't reached the last word
         if current_guess < len(words):
+            #See if we got it
             if words[current_guess] in [password + '\n', password]:
                 success = True
                 break
+            #Check if capitalization will fix it
             elif words[current_guess].capitalize() in [password + '\n', password]:
                 success = True
                 break
+        #Password is not in list
         else:
             break
         current_guess += 1
     if success:
-        end_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second 
-        running = False
+        #Print victory message plus time it took
+        end_time = now_seconds()
         return f'found {password} after {current_guess} loops, {end_time - start_time} seconds'
     else:
-        end_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second 
-        running = False
+        #Print defeat message plus time it took
+        end_time = now_seconds()
         return f'could not find {password} after {current_guess} loops, {end_time - start_time} seconds'
 
+#Looks for a word in dictionary.txt
 def test_words(password: str):
-    running = True
-    start_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second
+    #Log start time
+    start_time = now_seconds()
+
+    #Open file, copy it to a list, and close it
     f = open('dictionary.txt')
     words = f.readlines()
     f.close()
     
+    #Initialize loop
     success = False
     current_guess = 0
     while True:
+        #Ensure we haven't reached the last word
         if current_guess < len(words):
+            #See if we guessed correctly
             if words[current_guess] in [password + '\n', password]:
                 success = True
                 break
+            #Check if capitalization will fix it
             elif words[current_guess].capitalize() in [password + '\n', password]:
                 success = True
                 break
+        #Password is not in list
         else:
             break
         current_guess += 1
     if success:
-        end_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second 
-        running = False
+        #Print victory message plus time it took
+        end_time = now_seconds()
         return f'found {password} after {current_guess} loops, {end_time - start_time} seconds'
     else:
-        end_time = datetime.now().hour * 360 + datetime.now().minute * 60 + datetime.now().second 
-        running = False
+        #Print defeat message plus time it took
+        end_time = now_seconds()
         return f'could not find {password} after {current_guess} loops, {end_time - start_time} seconds'
+
+#Test for dictionary word + numbers
+def test_word_num(password: str, digits: int):
+    pass
+
+#Test randomly
+def random_test(password: str, timeout: int):
+    pass
--- Main.py
+++ Main.py
@@ -1,56 +1,78 @@
 from Gorithms import *
 import tkinter as tk
 
+#Init vars for changing labels
 last_common_result = ''
 last_dict_result = ''
 last_random_result = ''
 
+#Handle button click
 def handle_click():
+    #Ensure the user gave a password
     if password_ent.get() == '':
         return
+    #Ensure the timeout value is valid
     if timeout_ent.get() == '' or int(timeout_ent.get()) == None:
         status_lbl.config(text='Please enter a valid timeout value')
         return
 
+    #Disable input
     password_ent.config(state=tk.DISABLED)
+    timeout_ent.config(state=tk.DISABLED)
     btn.config(state=tk.DISABLED)
+
+    #Flavor
     status_lbl.config(text='Working...')
 
+    #Run our algs
     last_common_result = test_commons(password_ent.get())
     last_dict_result = test_words(password_ent.get())
 
+    #Display results
     status_lbl.config(text='Please enter a password and timeout value')
     results_lbl.config(text=f'Common Password Alg {last_common_result}\nDictionary Search Alg {last_dict_result}\nRandom Alg {last_random_result}')
+    
+    #Enable input
     password_ent.config(state=tk.NORMAL)
+    timeout_ent.config(state=tk.NORMAL)
     btn.config(state=tk.NORMAL)
 
-
+#Init window
 window = tk.Tk()
 window.resizable(False, False)
 window.title('Password Guesser')
 
+#Results label
 results_lbl = tk.Label(master=window, height=5, text=f'Common Password Alg {last_common_result}\nDictionary Search Alg {last_dict_result}\nRandom Alg {last_random_result}', justify=tk.LEFT)
 results_lbl.grid(row=0, column=1, pady=10)
 
+#Password prompt
 password_lbl = tk.Label(master=window, text='Password: ', justify=tk.LEFT)
 password_lbl.grid(row=1, column=0, pady=10)
 
+#Password entry box
 password_ent = tk.Entry(master=window, width=50)
 password_ent.grid(row=1, column=1, pady=10)
 
+#Timeout prompt
 timeout_lbl = tk.Label(master=window, text='Timeout (s): ', justify=tk.LEFT)
 timeout_lbl.grid(row=2, column=0, pady=10)
 
+#Timeout entry box
 timeout_ent = tk.Entry(master=window, width=50)
 timeout_ent.grid(row=2, column=1, pady=10)
 
+#Start button
 btn = tk.Button(master=window, relief=tk.RAISED, text='Start Guessing', command=handle_click)
 btn.grid(row=1, column=2, padx=10)
 
+#Status label
 status_lbl = tk.Label(master=window, text='Please enter a password and timeout value', width=70)
 status_lbl.grid(row=3, column=1, pady=10)
 
+#Empty, for looks only
 empty_frm = tk.Frame(master=window, height=20, width=20)
 empty_frm.grid(row=4, column=1, pady=10)
 
+#Start
 window.mainloop()
