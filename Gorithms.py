69971cfbec85e5e9da39de4d5c5341e5447ddf9e
AtomSmasher28 <33180708+AtomSmasher28@users.noreply.github.com>
2021-01-16 12:46:56 -0600

    Need to fix word+num alg

--- Gorithms.py
+++ Gorithms.py
@@ -1,12 +1,19 @@
-from datetime import datetime
-import random
+from datetime import datetime, time
+import itertools
 
 special_chars = '`~!@#$%^&*()-=_+[]\\{}|;\':",./<>?'
-chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz' + special_chars
+num_chars = '1234567890'
+chars = ' AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz' + num_chars + special_chars
 
 #Helpful, gets current time in milliseconds
 def now_mseconds():
     return datetime.now().hour * 360 * 1000 + datetime.now().minute * 60 * 1000 + datetime.now().second * 1000 + round(datetime.now().microsecond / 1000)
+
+#Got from Stack Overflow
+def iter_all_strings():
+    for size in itertools.count(1):
+        for s in itertools.product(chars, repeat=size):
+            yield ''.join(s)
 
 #Looks for a word in a file
 def test_file(password: str, input_file: str):
@@ -39,11 +46,11 @@ def test_file(password: str, input_file: str):
     if success:
         #Print victory message plus time it took
         end_time = now_mseconds()
-        return f'found {password} after {current_guess} loops, {end_time - start_time} milliseconds'
+        return f'found {password} after {current_guess} loops, {(end_time - start_time) / 1000.0} seconds'
     else:
         #Print defeat message plus time it took
         end_time = now_mseconds()
-        return f'could not find {password} after {current_guess} loops, {end_time - start_time} milliseconds'
+        return f'could not find {password} after {current_guess} loops, {(end_time - start_time) / 1000.0} seconds'
 
 #Test for dictionary word + numbers
 def test_word_num(password: str, input_file: str, timeout: int):
@@ -63,31 +70,21 @@ def test_word_num(password: str, input_file: str, timeout: int):
         #Ensure we haven't reached the last word
         if current_guess < len(words):
             #See if we guessed correctly
-            if words[current_guess].replace('\n', '') == password:
-                success = True
-                break
-            #Check if capitalization will make our guess correct
-            elif words[current_guess].replace('\n', '').capitalize() == password:
+            if password in [words[current_guess].replace('\n', ''), words[current_guess].replace('\n', '').capitalize()]:
                 success = True
                 break
             else:
                 count += 1
                 #Now we try appending numbers
                 for i in range(9):
-                    if words[current_guess].replace('\n', '') + str(i) == password:
-                        success = True
-                        break
-                    elif words[current_guess].replace('\n', '').capitalize() + str(i) == password:
+                    if password in [words[current_guess].replace('\n', '') + str(i), words[current_guess].replace('\n', '').capitalize() + str(i)]:
                         success = True
                         break
                     else:
                         count += 1
                         #And special chars
                         for j in range(len(special_chars)):
-                            if words[current_guess].replace('\n', '') + str(i) + special_chars[j] == password:
-                                success = True
-                                break
-                            elif words[current_guess].replace('\n', '').capitalize() + str(i) + special_chars[j] == password:
+                            if password in [words[current_guess].replace('\n', '') + str(i) + special_chars[j], words[current_guess].replace('\n', '').capitalize() + str(i) + special_chars[j]]:
                                 success = True
                                 break
                         if now_mseconds() - start_time >= timeout * 1000:
@@ -103,16 +100,32 @@ def test_word_num(password: str, input_file: str, timeout: int):
     if success:
         #Print victory message plus time it took
         end_time = now_mseconds()
-        return f'found {password} after {count} loops, {end_time - start_time} milliseconds'
+        return f'found {password} after {count} loops, {(end_time - start_time) / 1000.0} seconds'
     else:
         #Print defeat message plus time it took
         end_time = now_mseconds()
-        return f'could not find {password} after {count} loops, {end_time - start_time} milliseconds'
+        return f'could not find {password} after {count} loops, {(end_time - start_time) / 1000.0} seconds'
 
 #Test randomly
 def random_test(password: str, timeout: int):
-    guess = ['']
-
+    #Log start time
+    start_time = now_mseconds()
+    
     success = False
-    for i in range(len(char)):
-        
+    count = 0
+    for guess in iter_all_strings():
+        if guess == password:
+            success = True
+            break
+        if now_mseconds() - start_time >= timeout * 1000:
+            break
+        else:
+            count += 1
+    if success:
+        #Print victory message plus time it took
+        end_time = now_mseconds()
+        return f'found {password} after {count} loops, {(end_time - start_time) / 1000.0} seconds'
+    else:
+        #Print defeat message plus time it took
+        end_time = now_mseconds()
+        return f'could not find {password} after {count} loops, {(end_time - start_time) / 1000.0} seconds'
--- Main.py
+++ Main.py
@@ -1,80 +1,133 @@
+from os import stat
 from Gorithms import *
+import winsound
 import tkinter as tk
 
-#Init vars for changing labels
-common_result = ''
-dict_result = ''
-dict_num_result = ''
-random_result = ''
+status_lbl_text = 'Please enter a password and timeout value'
 
 #Handle button click
 def handle_click():
+    #Init vars for changing labels
+    common_result = 'was not run'
+    dict_result = 'was not run'
+    dict_num_result = 'was not run'
+    random_result = 'was not run'
+
     #Ensure the user gave a password
     if password_ent.get() == '':
         return
     #Ensure the timeout value is valid
     if timeout_ent.get() == '' or int(timeout_ent.get()) == None:
-        status_lbl.config(text='Please enter a valid timeout value')
+        status_lbl_text = 'Please enter a valid timeout value'
+        status_lbl.config(text=status_lbl_text)
         return
 
     #Disable input
     password_ent.config(state=tk.DISABLED)
     timeout_ent.config(state=tk.DISABLED)
+    cps_cbx.config(state=tk.DISABLED)
+    ds_cbx.config(state=tk.DISABLED)
+    dns_cbx.config(state=tk.DISABLED)
+    rnd_cbx.config(state=tk.DISABLED)
     btn.config(state=tk.DISABLED)
 
     #Flavor
-    status_lbl.config(text='Calculating...')
+    status_lbl_text='Calculating...'
+    status_lbl.config(text=status_lbl_text)
 
     #Run our algs
-    common_result = test_file(password_ent.get(), 'commonwords.txt')
-    dict_result = test_file(password_ent.get(), 'dictionary.txt')
-    dict_num_result = test_word_num(password_ent.get(), 'dictionary.txt', int(timeout_ent.get()))
+    if run_cps.get():
+        common_result = test_file(password_ent.get(), 'commonwords.txt')
+    if run_ds.get():
+        dict_result = test_file(password_ent.get(), 'dictionary.txt')
+    if run_dns.get():
+        dict_num_result = test_word_num(password_ent.get(), 'dictionary.txt', int(timeout_ent.get()))
+    if run_rnd.get():
+        random_result = random_test(password_ent.get(), int(timeout_ent.get()))
+    
+    #Play sound
+    winsound.Beep(440, 100)
 
     #Display results
-    status_lbl.config(text='Please enter a password and timeout value')
-    results_lbl.config(text=f'Common Password Search {common_result}\nDictionary Search {dict_result}\nDictionary and Number Search {dict_num_result}\nRandom Alg {random_result}')
-    
+    status_lbl_text = 'Please enter a password and timeout value'
+    status_lbl.config(text=status_lbl_text)
+    cps_results_lbl.config(text=f'Common Password Search {common_result}')
+    ds_results_lbl.config(text=f'Dictionary Search {dict_result}')
+    dns_results_lbl.config(text=f'Dictionary and Number Search {dict_num_result}')
+    rnd_results_lbl.config(text=f'Random Search {random_result}')
+
     #Enable input
     password_ent.config(state=tk.NORMAL)
     timeout_ent.config(state=tk.NORMAL)
     btn.config(state=tk.NORMAL)
+    cps_cbx.config(state=tk.NORMAL)
+    ds_cbx.config(state=tk.NORMAL)
+    dns_cbx.config(state=tk.NORMAL)
+    rnd_cbx.config(state=tk.NORMAL)
 
 #Init window
 window = tk.Tk()
 window.resizable(False, False)
 window.title('Password Guesser')
 
-#Results label
-results_lbl = tk.Label(master=window, height=5, text=f'Common Password Search {common_result}\nDictionary Search {dict_result}\nDictionary and Number Search {dict_num_result}\nRandom Alg {random_result}', justify=tk.LEFT)
-results_lbl.grid(row=0, column=1, pady=10)
+run_cps = tk.BooleanVar()
+run_ds = tk.BooleanVar()
+run_dns = tk.BooleanVar()
+run_rnd = tk.BooleanVar()
+
+#Checkboxes
+cps_cbx = tk.Checkbutton(master=window, variable=run_cps, onvalue=True, offvalue=False)
+cps_cbx.grid(row=1, column=0, sticky='e')
+
+ds_cbx = tk.Checkbutton(master=window, variable=run_ds, onvalue=True, offvalue=False)
+ds_cbx.grid(row=2, column=0, sticky='e')
+
+dns_cbx = tk.Checkbutton(master=window, variable=run_dns, onvalue=True, offvalue=False)
+dns_cbx.grid(row=3, column=0, sticky='e')
+
+rnd_cbx = tk.Checkbutton(master=window, variable=run_rnd, onvalue=True, offvalue=False)
+rnd_cbx.grid(row=4, column=0, sticky='e')
+
+#Results labels
+cps_results_lbl = tk.Label(master=window, height=1, text=f'Common Password Search', justify=tk.LEFT)
+cps_results_lbl.grid(row=1, column=1, sticky='w')
+
+ds_results_lbl = tk.Label(master=window, height=1, text=f'Dictionary Search', justify=tk.LEFT)
+ds_results_lbl.grid(row=2, column=1, sticky='w')
+
+dns_results_lbl = tk.Label(master=window, height=1, text=f'Dictionary and Number Search', justify=tk.LEFT)
+dns_results_lbl.grid(row=3, column=1, sticky='w')
+
+rnd_results_lbl = tk.Label(master=window, height=1, text=f'Random Search', justify=tk.LEFT)
+rnd_results_lbl.grid(row=4, column=1, sticky='w')
 
 #Password prompt
 password_lbl = tk.Label(master=window, text='Password: ', justify=tk.LEFT)
-password_lbl.grid(row=1, column=0, pady=10)
+password_lbl.grid(row=5, column=0, pady=10, sticky='e')
 
 #Password entry box
 password_ent = tk.Entry(master=window, width=50)
-password_ent.grid(row=1, column=1, pady=10)
+password_ent.grid(row=5, column=1, pady=10, sticky='w')
 
 #Timeout prompt
 timeout_lbl = tk.Label(master=window, text='Timeout (seconds): ', justify=tk.LEFT)
-timeout_lbl.grid(row=2, column=0, pady=10)
+timeout_lbl.grid(row=6, column=0, pady=10, sticky='e')
 
 #Timeout entry box
 timeout_ent = tk.Entry(master=window, width=50)
-timeout_ent.grid(row=2, column=1, pady=10)
+timeout_ent.grid(row=6, column=1, pady=10, sticky='w')
 
 #Start button
 btn = tk.Button(master=window, relief=tk.RAISED, text='Start Guessing', command=handle_click)
-btn.grid(row=1, column=2, padx=10)
+btn.grid(row=5, column=2, padx=10)
 
 #Status label
-status_lbl = tk.Label(master=window, text='Please enter a password and timeout value', width=70)
-status_lbl.grid(row=3, column=1, pady=10)
+status_lbl = tk.Label(master=window, text=status_lbl_text, width=70)
+status_lbl.grid(row=7, column=1, pady=10)
 
 #Empty, for looks only
 empty_frm = tk.Frame(master=window, height=20, width=20)
-empty_frm.grid(row=4, column=1, pady=10)
+empty_frm.grid(row=8, column=1, pady=10)
 
 #Start
 window.mainloop()
