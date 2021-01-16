d7ab6e0f465ac1040488361baa4610b4a5eeea7e
John Walker <johnwalker00@gmail.com>
2021-01-16 17:17:43 -0600

    split the testfile into 200

---
+++ testing/filesplitter.py
@@ -0,0 +1,12 @@
+lines_per_file = 5000
+smallfile = None
+with open('testing/testpasswords.txt') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = 'testing/testpasswords_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
